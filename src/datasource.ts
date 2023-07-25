2873c5c17bf39c77e8b1ee83d54a454c5132e01c
Ivana Huckova <ivana.huckova@gmail.com>
2023-07-25 17:40:35 +0200

    Use logs data plane data frames for results

--- src/datasource.ts
+++ src/datasource.ts
@@ -5,6 +5,7 @@ import {
   DataSourceInstanceSettings,
   MutableDataFrame,
   FieldType,
+  DataFrameType,
 } from '@grafana/data';
 
 import { MyQuery, MyDataSourceOptions } from './types';
@@ -15,18 +16,35 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
   }
 
   async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
-    const { range } = options;
+    const { range, targets } = options;
     const from = range!.from.valueOf();
     const to = range!.to.valueOf();
 
-    // Return a constant for each query.
+    const searchWords = targets.map((target) => target.queryText)
+
     const data = options.targets.map((target) => {
       return new MutableDataFrame({
         refId: target.refId,
-        fields: [
-          { name: 'Time', values: [from, to], type: FieldType.time },
-          { name: 'Value', values: [target.constant, target.constant], type: FieldType.number },
+          fields: [
+            { name: 'timestamp', type: FieldType.time, values: [from, from + 1000, from + 20000, from + 3000, to] },
+            { name: 'body', type: FieldType.string, values: ['message one', 'message two', 'message three', 'message four', 'message five'] },
+            { name: 'severity', type: FieldType.string, values: ['critical', 'error', 'warning', 'debug', 'trace'] },
+            { name: 'id', type: FieldType.string, values: ['xxx-001', 'xyz-002', 'xyz-003', 'xyz-004', 'xyz-005'] },
+            {
+              name: 'attributes',
+              type: FieldType.other,
+              values: [ {hello: 'world'}, { hello: 'world', abc: 'def' }, { hello: 'world', foo: 123.45, bar: ['yellow', 'red'], baz: { name: 'alice' } }, { hello: 'world' }, { hello: 'world'} ],
+            },
         ],
+        meta: {
+          type: DataFrameType.LogLines,
+          preferredVisualisationType: 'logs',
+          custom: {
+            limit: 10,
+            error: "test error message",
+            searchWords,
+          }
+        }
       });
     });
 
--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,11 @@
 import { DataQuery, DataSourceJsonData } from '@grafana/data';
 
 export interface MyQuery extends DataQuery {
-  queryText?: string;
-  constant: number;
+  queryText: string;
 }
 
 export const DEFAULT_QUERY: Partial<MyQuery> = {
-  constant: 6.5,
+  queryText: ''
 };
 
 /**
